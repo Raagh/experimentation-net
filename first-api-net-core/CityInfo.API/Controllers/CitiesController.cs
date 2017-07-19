e21b093b9517924691236f889bca12e4cd438fe7
Patricio Ferraggi <pattferraggi@gmail.com>
2017-07-19 16:09:53 -0300

    first-api-net-core: Updated City Info

---
+++ first-api-net-core/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+
+        [HttpGet()]
+        public JsonResult GetCites()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+    }
+}
